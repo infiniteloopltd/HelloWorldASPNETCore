4fd1a755230ab164588c535df91be934acec6fed
fiach <fiach.reid@gmail.com>
2018-11-25 13:49:54 +0000

    First version

---
+++ HelloWorldASPNETCore/Default.aspx
@@ -0,0 +1,12 @@
+﻿<%@ Page Language="C#" Inherits="HelloWorldASPNETCore.Default" %>
+<!DOCTYPE html>
+<html>
+<head runat="server">
+	<title>default</title>
+</head>
+<body>
+	<form id="form1" runat="server">
+            <%= IP %>
+	</form>
+</body>
+</html>
---
+++ HelloWorldASPNETCore/Default.aspx.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Net;
+using System.Web;
+using System.Web.UI;
+
+namespace HelloWorldASPNETCore
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        protected string IP = "";
+
+        protected void Page_Load(object source, EventArgs e) 
+        {
+            WebClient wc = new WebClient();
+            this.IP = wc.DownloadString("http://www.icanhazip.com");
+        }
+
+    }
+}
